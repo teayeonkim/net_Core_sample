3723a919a7d1361fbae24305304cb4ca33d5cc68
teayeonkim <tmakdlf1522@nver.com>
2021-04-20 18:37:23 +0900

    김태연

---
+++ net_Core_sample/BaseFramework/AppConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseFramework
+{
+    public class AppConfiguration
+    {
+        /* Web 프로젝트 설정값 수취 패키지 설치 진행
+            Microsoft.Extensions.Configuration
+            Microsoft.Extensions.Configuration.Abstractions 
+            Microsoft.Extensions.Configuration.Json
+            
+            패키지 설치 필요
+        */
+        //클립 암호화키 AES
+        public readonly string _CryptoKey_AES = string.Empty;
+        //SIte 엔티티
+        public readonly string _Site_Clipservice = string.Empty;
+        //디버깅 여부
+        public readonly string _Mode = string.Empty;
+        
+        public AppConfiguration()
+        {
+            
+            var configurationBuilder = new ConfigurationBuilder();
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+   
+            #if DEBUG //디버깅 모드
+                path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Development.json");
+            #endif
+            configurationBuilder.AddJsonFile(path, optional: true, reloadOnChange: true);
+            var root = configurationBuilder.Build();
+
+            //암호화키 추출
+            _CryptoKey_AES = root.GetSection("CryptoKey_AES").Value;
+
+            //사이트 엔티티 추출
+            _Site_Clipservice = root.GetSection("ConnectiongStrings:Site_Clipservice").Value;
+
+            //디버깅 모드여부 추출
+            _Mode = root.GetSection("Mode").Value;
+        }
+
+        //암호화키 수취
+        public string CryptoKey_AES
+        {
+            get => _CryptoKey_AES;
+        }
+
+        //사이트 엔티티 추출
+        public string Site_Clipservice
+        {
+            get => _Site_Clipservice;
+        }
+
+        //디버깅 여부 
+        public string Mode
+        {
+            get => _Mode;
+        }
+
+
+    }
+}
---
+++ net_Core_sample/BaseFramework/Security/CryptoUtil.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BaseFramework.Security
+{
+	public class CryptoUtil
+	{
+		//웹 appsettings.Json 추출 
+		public static AppConfiguration AppConfig = new AppConfiguration();
+
+		#region 기본 암호화는 AES로 연결
+		//암호화
+		public static string Encrypt(string val)
+		{
+			string key = AppConfig.CryptoKey_AES;
+
+			return Encrypt(val, key);
+		}
+		public static string Encrypt(object val) { return val == null ? "" : Encrypt(val.ToString()); }
+		public static string Encrypt(string val, string key) { return EncAes(val, key); }
+		public static string Encrypt(object val, string key) { return val == null ? "" : Encrypt(val.ToString(), key); }
+
+		//복호화
+		public static string Decrypt(string val)
+		{
+			string key = AppConfig.CryptoKey_AES;
+
+			return Decrypt(val, key);
+		}
+		public static string Decrypt(object val) { return val == null ? "" : Decrypt(val.ToString()); }
+		public static string Decrypt(string val, string key) { return DecAes(val, key); }
+		public static string Decrypt(object val, string key) { return val == null ? "" : Decrypt(val.ToString(), key); }
+		#endregion
+
+		#region Aes 암호화
+		//암호화
+		public static string EncAes(string str)
+		{
+			//AppConfiguration AppConfig = new AppConfiguration();
+
+			string key = AppConfig.CryptoKey_AES;
+			return EncAes(str, key);
+		}
+
+		public static String EncAes(String Input, String key)
+		{
+			var aes = System.Security.Cryptography.Aes.Create();
+			aes.KeySize = 256;
+			aes.BlockSize = 128;
+			aes.Mode = CipherMode.CBC;
+			aes.Padding = PaddingMode.PKCS7;
+			aes.Key = Encoding.Default.GetBytes(key.Substring(0, 32));
+			aes.IV = Encoding.Default.GetBytes(key.Substring(0, 16));
+			//aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			//aes.IV = new byte[] { 0, 1, 0, 3, 2, 2, 8, 0, 2, 6, 4, 0, 8, 0, 3, 0 };
+
+			var encrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+			byte[] xBuff = null;
+			using (var ms = new MemoryStream())
+			{
+				using (var cs = new CryptoStream(ms, encrypt, CryptoStreamMode.Write))
+				{
+					byte[] xXml = Encoding.UTF8.GetBytes(Input);
+					cs.Write(xXml, 0, xXml.Length);
+				}
+
+				xBuff = ms.ToArray();
+			}
+
+			String Output = Convert.ToBase64String(xBuff);
+			return Output;
+		}
+
+
+		//복호화
+		public static string DecAes(string str)
+		{
+			AppConfiguration a = new AppConfiguration();
+			string key = a.CryptoKey_AES;
+			return DecAes(str, key);
+		}
+
+		public static String DecAes(String Input, String key)
+		{
+			var aes = System.Security.Cryptography.Aes.Create();
+			aes.KeySize = 256;
+			aes.BlockSize = 128;
+			aes.Mode = CipherMode.CBC;
+			aes.Padding = PaddingMode.PKCS7;
+			aes.Key = Encoding.Default.GetBytes(key.Substring(0, 32));
+			aes.IV = Encoding.Default.GetBytes(key.Substring(0, 16));
+			//aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			//aes.IV = new byte[] { 0, 1, 0, 3, 2, 2, 8, 0, 2, 6, 4, 0, 8, 0, 3, 0 };
+
+			var decrypt = aes.CreateDecryptor();
+			byte[] xBuff = null;
+			using (var ms = new MemoryStream())
+			{
+				using (var cs = new CryptoStream(ms, decrypt, CryptoStreamMode.Write))
+				{
+					byte[] xXml = Convert.FromBase64String(Input);
+					cs.Write(xXml, 0, xXml.Length);
+				}
+
+				xBuff = ms.ToArray();
+			}
+
+			String Output = Encoding.UTF8.GetString(xBuff);
+			return Output;
+		}
+
+		#endregion
+	}
+}
