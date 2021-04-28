0569c70e3682c33f08d2dc514aa450717c923e11
김태연 <78404151+teayeonkim@users.noreply.github.com>
2021-04-28 17:55:33 +0900

    Json 추Update README.md

--- README.md
+++ README.md
@@ -23,3 +23,5 @@
           중복 파일 체크 및 파일명 변경
           이미지 리사이징 기능 
     
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fteayeonkim%2Fnet_Core_sample&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
---
+++ net_Core_sample/BaseFramework/Utilities/JsonHelper.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseFramework.Utilities
+{
+	public class JsonHelper
+	{
+		public static string Serialize(object obj, JsonSerializerSettings setting)
+		{
+			return JsonConvert.SerializeObject(obj, setting);
+		}
+		public static string Serialize(object obj)
+		{
+			return Serialize(obj, new JsonSerializerSettings { DateFormatHandling = DateFormatHandling.MicrosoftDateFormat });
+		}
+
+
+
+		public static T Deserialize<T>(string jsonStr, JsonSerializerSettings setting)
+		{
+			if (string.IsNullOrEmpty(jsonStr)) return default(T);
+
+			return JsonConvert.DeserializeObject<T>(jsonStr, setting);
+		}
+		public static T Deserialize<T>(string jsonStr)
+		{
+			return Deserialize<T>(jsonStr, new JsonSerializerSettings { DateFormatHandling = DateFormatHandling.MicrosoftDateFormat });
+		}
+
+		public static List<T> Deserialize<T>(List<T> list)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
